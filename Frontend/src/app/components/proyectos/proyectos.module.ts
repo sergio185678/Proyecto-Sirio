977d9d7719f2bf71c5227536e8749fdadd6b0d8c
PizzaaDinner <89035983+gatitoEsponjosito@users.noreply.github.com>
2023-06-25 19:14:08 -0500

    added logo and navbar

--- Frontend/src/app/components/proyectos/proyectos.module.ts
+++ Frontend/src/app/components/proyectos/proyectos.module.ts
@@ -5,17 +5,13 @@ import { ProyectosRoutingModule } from './proyectos-routing.module';
 import { BuscadorComponent } from './buscador/buscador.component';
 import { ProjectHomeComponent } from './project-home/project-home.component';
 import { Proyecto1Component } from './proyecto1/proyecto1.component';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
 
 
 @NgModule({
   declarations: [
     BuscadorComponent,
     ProjectHomeComponent,
-    Proyecto1Component,
-    NavbarComponent,
-    FooterComponent
+    Proyecto1Component
   ],
   imports: [
     CommonModule,
