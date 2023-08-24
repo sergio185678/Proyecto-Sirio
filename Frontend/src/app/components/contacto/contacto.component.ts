636afc4900b6b048181f33b5bb0cd739db8f35b5
Ergotox <Diegojsf17@gmail.com>
2023-08-24 00:44:42 -0500

    feat: send email function implemented

--- Frontend/src/app/components/contacto/contacto.component.ts
+++ Frontend/src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contacto',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-  name='';
-  email='';
-  message='';
+  correo = {
+    name: '',
+    email:'',
+    message: ""
+  };
+
+  constructor(private http: HttpClient) {}
+
+  enviarCorreo() {
+    if (!this.isEmailValid()) {
+      this.clearEmailField();
+      window.alert("El correo electrónico no es válido. Por favor, ingréselo correctamente.");
+      return;
+    }else{
+      this.http.post('https://formcarry.com/s/IZxYOVynAG',  this.correo)
+        .subscribe(
+          response => {
+            console.log('Respuesta de la llamada AJAX:', response);
+            window.alert("Se ha realizado el envio correctamente");
+          },
+          error => {
+            console.error('Error en la llamada AJAX:', error);
+          }
+        );
+
+      this.clearAllFields();
+    }
+
+  }
+  isEmailValid(): boolean {
+    return this.correo.email.includes("@") && this.correo.email.includes(".");
+  }
+
+  clearEmailField() {
+    this.correo.email = "";
+  }
+  clearAllFields(){
+    this.correo.name = "";
+    this.correo.email = "";
+    this.correo.message = "";
+  }
 }
