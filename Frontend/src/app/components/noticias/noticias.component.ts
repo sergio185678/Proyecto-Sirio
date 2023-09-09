1575d8abb03fea1fac130967f82a6ebf7aaba4e1
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-09-09 12:35:33 -0500

    feat: consumo de service para la seccion noticias, iteracion de noticias para la vista.

--- Frontend/src/app/components/noticias/noticias.component.ts
+++ Frontend/src/app/components/noticias/noticias.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import * as http from "http";
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrls: ['./noticias.component.css']
 })
-export class NoticiasComponent {
+export class NoticiasComponent implements OnInit{
+  info : any = {
+  };
+
+  cargada = false;
+  constructor(private http: HttpClient) {
+    console.log("Todo okay")
+    http.get("../../assets/noticias_list.json")
+      .subscribe( data => {
+        this.info = data;
+        this.cargada = true;
+      });
+  }
+  ngOnInit() {
+  }
 
 }
--- Frontend/src/app/models/noticias.ts
+++ Frontend/src/app/models/noticias.ts
@@ -1,13 +1,14 @@
 export class Noticias{
   id:number;
+  nombre?:string;
   descripcion?:string;
   url_img?:string;
   url_noticia?:string;
-  constructor(id:number,descripcion:string,url_img:string,url_noticia:string){
+  constructor(id:number,nombre:string, descripcion:string,url_img:string,url_noticia:string){
     this.id=id;
+    this.nombre= nombre;
     this.descripcion=descripcion;
     this.url_img=url_img;
     this.url_noticia=url_noticia;
-
   }
 }
