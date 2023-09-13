97621cde9ef5496e888bdaf9d050cadc28666223
Ibrahim <u20191E650@upc.edu.pe>
2023-09-13 18:21:07 -0500

    small changes

--- Frontend/src/app/components/acerca-directorio/acerca-directorio.component.ts
+++ Frontend/src/app/components/acerca-directorio/acerca-directorio.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acerca-directorio',
   templateUrl: './acerca-directorio.component.html',
   styleUrls: ['./acerca-directorio.component.css']
 })
-export class AcercaDirectorioComponent {
+export class AcercaDirectorioComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    let menuToggle = document.querySelector('.toggle');
+    let menu = document.querySelector('.menu');
+    menuToggle?.addEventListener('click', () => {
+      menu?.classList?.toggle('active');
+      menuToggle?.classList?.toggle('active');
+    });
+  }
 }
