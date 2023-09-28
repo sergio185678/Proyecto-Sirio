01305f4cbc3d802c823551ab265c04ab0a0ef9f8
Ibrahim <u20191E650@upc.edu.pe>
2023-09-28 18:24:30 -0500

    pizza section for the menu-areas

--- Frontend/src/app/components/acerca-directorio/acerca-directorio.component.ts
+++ Frontend/src/app/components/acerca-directorio/acerca-directorio.component.ts
@@ -6,17 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acerca-directorio.component.css']
 })
 export class AcercaDirectorioComponent implements OnInit {
+    numColors: number = 8;
+    colorsUsed: string[] = ['#0019FD', '#DE0D98', '#DEC800', '#2AC11D', '#03C1F5', '#1B1B1A', '#1B1B1B', '#1B1B1C'];
+    colors: string[] = this.colorsUsed;
+    highlightedColor: string | null = null;
+    constructor() { }
+    ngOnInit(): void {
+      let menuToggle = document.querySelector('.toggle');
+      let animation = document.querySelector('.animation-rows');
+      let menu = document.querySelector('.AreasMenu');
+      let container = document.querySelector('.container-box');
+      menuToggle?.addEventListener('click', () => {
+        menu?.classList?.toggle('active');
+        animation?.classList?.toggle('noDisplay');
+        menuToggle?.classList?.toggle('active');
+        container?.classList?.toggle('active');
+        container?.classList?.toggle('inactive');
+        this.calculateDrawingPizzaColor();
+      });
+    }
+    chooseColor(color: string, colors: string[]): void {
+      this.colors = this.colorsUsed.map((usedColor) => usedColor === color ? usedColor : '#060A12');
+    }
 
-  constructor() { }
+    clearColors(): void {
+      this.calculateDrawingPizzaColor('transparent');
+    }
 
-  ngOnInit(): void {
-    let menuToggle = document.querySelector('.toggle');
-    let animation = document.querySelector('.animation');
-    let menu = document.querySelector('.menu');
-    menuToggle?.addEventListener('click', () => {
-      menu?.classList?.toggle('active');
-      menuToggle?.classList?.toggle('active');
-      animation?.classList?.toggle('noDisplay');
-    });
-  }
+    calculateDrawingPizzaColor(colorChosen: string='transparent'): void {
+      this.chooseColor(colorChosen, this.colors);
+      let colorStops = '';
+      let angleStep = 360 / this.numColors;
+      angleStep = (angleStep / 360) * 100 / 2;
+      let currentAngle = 0;
+      for (let i = 0; i < this.numColors * 2; i++) {
+        const color = this.colors[Math.floor(i !== 0 ? (i + 1) / 2 : i) % this.colors.length];
+        const start = currentAngle;
+        currentAngle += angleStep;
+        const end = currentAngle;
+        colorStops += `${color} ${start}% ${end}%`;
+        if (i !== this.numColors * 2 - 1) {
+          colorStops += ', ';
+        }
+      }
+      document.documentElement.style.setProperty('--color-stops', colorStops);
+    }
+    highlightSection(color: string): void {
+      this.calculateDrawingPizzaColor(color);
+      this.highlightedColor = color;
+    }
+    clearHighlight(): void {
+      if (this.highlightedColor) {
+        this.clearColors();// Reset to default (e.g., transparent)
+        this.highlightedColor = null;
+      }
+    }
 }
